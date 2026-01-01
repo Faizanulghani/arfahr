0b5523f4d90e075fe579cea807dbff74a1b4213e
Faizanulghani <faizanulghani1@gmail.com>
2026-01-01 14:50:44 +0500

    fix year

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -297,7 +297,7 @@ const Index = () => {
             </div>
           </div>
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-            <p>&copy; 2024 AfraExpress International. All rights reserved.</p>
+            <p>&copy; 2026 AfraExpress International. All rights reserved.</p>
           </div>
         </div>
       </footer>
